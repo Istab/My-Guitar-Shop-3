72d77a66d0a7c5a49f05434ce160b84e51c0a80e
Istabraq <angel.ofmoons@gmail.com>
2017-07-24 04:14:18 -0400

    Display Session ID

--- cart_view.php
+++ cart_view.php
@@ -61,7 +61,7 @@
         <?php endif; ?>
         <p><a href=".?action=show_add_item">Add Item</a></p>
         <p><a href=".?action=empty_cart">Empty Cart</a></p>
-
+        <p>Session ID: <?php echo session_id(); ?></p>
     </main>
 </body>
 </html>
